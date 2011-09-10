50d472f0e5d92fb70d85d83430803c18e2e46a19
Alasdair Morrison <amorri40@gmail.com>
2011-09-10 13:22:18 +0100

    Initial commit of EnigmaJS

---
+++ EnigmaJSPluginSource/src/org/jsenigma/core/EnigmaJSFrame.java
@@ -0,0 +1,99 @@
+package org.jsenigma.core;
+/*
+ * Copyright (C) 2008, 2009 IsmAvatar <IsmAvatar@gmail.com>
+ * 
+ * This file is part of Enigma Plugin.
+ * 
+ * Enigma Plugin is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Enigma Plugin is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License (COPYING) for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JProgressBar;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+import org.jsenigma.messages.Messages;
+import org.lateralgm.main.LGM;
+
+public class EnigmaJSFrame extends JFrame
+	{
+	private static final long serialVersionUID = 1L;
+	public JTextArea ta;
+	public JProgressBar pb;
+
+	public EnigmaJSFrame()
+		{
+		super(Messages.getString("EnigmaFrame.TITLE")); //$NON-NLS-1$
+		setLocationRelativeTo(LGM.frame);
+		JPanel p = new JPanel(new BorderLayout());
+		ta = new JTextArea(10,40);
+		ta.setEditable(false);
+		p.add(new JScrollPane(ta,JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+				JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED),BorderLayout.CENTER);
+		pb = new JProgressBar();
+		pb.setStringPainted(true);
+		pb.setString(Messages.getString("EnigmaFrame.STARTING")); //$NON-NLS-1$
+		p.add(pb,BorderLayout.SOUTH);
+
+		setContentPane(p);
+		pack();
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		}
+
+	public void append(String text)
+		{
+		ta.append(text);
+		ta.setCaretPosition(ta.getDocument().getLength());
+		}
+
+	public void progress(int pos)
+		{
+		pb.setValue(pos);
+		}
+
+	
+	public void clear()
+		{
+		ta.setText(null);
+		}
+
+	
+	public void open()
+		{
+		setVisible(true);
+		}
+
+	
+	public void tip(String tip)
+		{
+		pb.setString(tip);
+		}
+
+	void progress(int pos, String tip)
+		{
+		progress(pos,tip,tip);
+		}
+
+	void progress(int pos, String tip, String text)
+		{
+		pb.setValue(pos);
+		pb.setString(tip);
+		ta.append(text + '\n'); //\n is internal representation of newlines, according to java
+		}
+	}
+
---
+++ EnigmaJSPluginSource/src/org/jsenigma/core/JSEnigmaRunner.java
@@ -0,0 +1,128 @@
+/*
+ * Copyright (C) 2011 Alasdair Morrison <amorri40@gmail.com>
+ * Copyright (C) 2008-2011 IsmAvatar <IsmAvatar@gmail.com>
+ * 
+ * This file is part of EnigmaJS Plugin.
+ * 
+ * EnigmaJS Plugin is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * EnigmaJS Plugin is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License (COPYING) for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.jsenigma.core;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import java.util.prefs.Preferences;
+
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+
+import org.jsenigma.messages.Messages;
+import org.lateralgm.main.LGM;
+
+
+
+
+public class JSEnigmaRunner implements ActionListener {
+
+	private JMenuItem run;
+	public static boolean SHUTDOWN;
+	public static final File WORKDIR = LGM.workDir.getParentFile();
+	public EnigmaJSFrame ef = new EnigmaJSFrame();
+
+	public JSEnigmaRunner() {
+		populateMenu();
+		Runtime.getRuntime().addShutdownHook(new Thread()
+		{
+			//Add hook so the updater knows when the application has been shutdown
+			public void run()
+				{
+				SHUTDOWN = true;
+				}
+		});
+		
+		new Thread()
+		{
+			public void run()
+				{
+				//Disable updates by removing plugins/shared/svnkit.jar (e.g. linux packages)
+				Preferences root = Preferences.userRoot().node("/org/enigma"); //$NON-NLS-1$
+				boolean rebuild = root.getBoolean("NEEDS_REBUILD",false); //$NON-NLS-1$
+				root.putBoolean("NEEDS_REBUILD",false); //$NON-NLS-1$
+				int updates = attemptUpdate(); //displays own error
+				if (updates == -1)
+					{
+					System.out.println("Update failed!");
+					return;
+					}
+				if (updates == 1)
+					{
+					System.out.println("Update succesful!");
+					}
+				}
+		}.start();
+	}
+
+	private void populateMenu() {
+		JMenu menu = new JMenu("EnigmaJS");
+		run = new JMenuItem("Compile to HTML5"); //$NON-NLS-1$
+		run.addActionListener(this);
+		menu.add(run);
+		LGM.frame.getJMenuBar().add(menu,1);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent ae) {
+		
+		Object s = ae.getSource();
+		if (s == run) html5();
+	}
+
+	private void html5() {
+		try {
+			JSEnigmaWriter.writeResources();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/** Attempts to checkout/update. Returns 0/1 on success, -1 on aborted checkout, -2 on failure, -3 on missing SvnKit */
+	public int attemptUpdate()
+		{
+		try
+			{
+			int up = EnigmaJSUpdater.checkForUpdates(ef);
+			if (EnigmaJSUpdater.needsRestart)
+				{
+				Preferences.userRoot().node("/org/enigma").putBoolean("NEEDS_REBUILD",true); //$NON-NLS-1$ //$NON-NLS-2$
+				JOptionPane.showMessageDialog(null,Messages.getString("EnigmaRunner.INFO_UPDATE_RESTART")); //$NON-NLS-1$
+				System.exit(120); //exit code 120 lets our launcher know to restart us.
+				}
+			return up;
+			}
+		/**
+		 * Usually you shouldn't catch an Error, however,
+		 * in this case we catch it to abort the module,
+		 * rather than allowing the failed module to cause
+		 * the entire program/plugin to fail
+		 */
+		catch (NoClassDefFoundError e)
+			{
+			System.err.println(Messages.getString("EnigmaRunner.WARN_UPDATE_MISSING_SVNKIT")); //$NON-NLS-1$
+			e.printStackTrace();
+			return -3;
+			}
+		}
+} //end of class
