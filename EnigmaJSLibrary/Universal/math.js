b588c66225483b7bf44f4ce893a20ff1363c7708
Alasdair Morrison <amorri40@gmail.com>
2011-09-21 01:27:07 +0100

    Added string functions

--- EnigmaJSLibrary/Universal/math.js
+++ EnigmaJSLibrary/Universal/math.js
@@ -1,6 +1,6 @@
 /*******************************************************************************
- * ***********************************************************\ Copyright (C)
- * 2011 Josh Ventura
+ *  Copyright (C) 2011 Josh Ventura
+ *  Copyright (C) 2011 Alasdair Morrison
  * 
  * This file is a part of the ENIGMA Development Environment.
  * 
